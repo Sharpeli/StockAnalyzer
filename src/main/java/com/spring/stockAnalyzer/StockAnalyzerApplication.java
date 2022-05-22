85759306f701e81ee5effff558d1b5e79265e74c
sharpeli <sharpel@sina.com>
2022-05-22 16:11:06 +0800

    add simple log viewer

--- src/main/java/com/spring/stockAnalyzer/StockAnalyzerApplication.java
+++ src/main/java/com/spring/stockAnalyzer/StockAnalyzerApplication.java
@@ -1,9 +1,12 @@
 package com.spring.stockAnalyzer;
 
+import com.logviewer.springboot.LogViewerSpringBootConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({LogViewerSpringBootConfig.class})
 public class StockAnalyzerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/spring/stockAnalyzer/application/stocks/SyncResult.java
+++ src/main/java/com/spring/stockAnalyzer/application/stocks/SyncResult.java
@@ -1,7 +1,9 @@
 package com.spring.stockAnalyzer.application.stocks;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 public class SyncResult {
     private boolean isSuccessful;
